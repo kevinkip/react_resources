ad754a84666d5592c1c1e93f56b14e49effb155c
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-20 11:03:08 -0700

    click events understanding

--- 02tut/src/Content.js
+++ 02tut/src/Content.js
@@ -7,12 +7,26 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   }
-  
+
+  const handleClick = () => {
+    console.log('You clicked it')
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText); // e.target === <button>Click it</button>
+  }
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={() => handleClick2('Kevin')}>Click it</button>
+        <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   )
 }
