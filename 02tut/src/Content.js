ec640b371c5bf6c4e8b4a3b1ef674d12de61d745
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-20 12:25:01 -0700

    lists and keys. fixed react-scripts.

--- 02tut/src/Content.js
+++ 02tut/src/Content.js
@@ -1,33 +1,77 @@
 import React from 'react'
 import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
-    const [name, setName] = useState('Kevin');
-    const [count, setCount] = useState(0);
+    const [items, setItems] = useState([
+      {
+          id: 1,
+          checked: true,
+          item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+      },
+      {
+          id: 2,
+          checked: false,
+          item: "Item 2"
+      },
+      {
+          id: 3,
+          checked: false,
+          item: "Item 3"
+      }
+  ]);
 
-  const handleNameChange = () => {
-    const names = ['Moose', 'Holly', 'Kevin'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
+  const handleCheck = (id) => {
+    /* console.log(`key: ${id}`); */
+    const listItems = items.map((item) =>
+      item.id === id 
+      ? { ...item, checked: !item.checked } 
+      : item
+    );
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   }
 
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  }
-
-  const handleClick2 = () => {
-    console.log(count);
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id); //return list with item.id that equals id. In other words, it filters out the ids that DON'T EQUAL id.
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   }
 
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {name}!
-        </p>
-        <button onClick={handleNameChange}>Change Name</button>
-        <button onClick={handleClick}>Click it</button>
-        <button onClick={handleClick2}>Click it</button>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li 
+            className="item" 
+            key={item.id}>
+              <input 
+              type="checkbox"
+              onChange={() => handleCheck(item.id)} //console.log ==> key: 2
+              checked={item.checked} 
+              />
+              <label htmlFor=""
+                onDoubleClick={() => handleCheck(item.id)}
+                style={(item.checked) ? { textDecoration:'line-through'} : null}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt 
+                role="button" 
+                tabIndex="0"
+                onClick={() => handleDelete(item.id)}
+                />
+            </li>
+          ))}
+        </ul>
+        ) : (
+          <p 
+          style={{ marginTop: '2rem' }}
+          >
+            Your list is empty.
+          </p>
+        )}
     </main>
   )
 }
