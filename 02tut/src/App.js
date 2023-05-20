b9903c046773107773954c63d819d9bf185170dd
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-19 22:30:16 -0700

    covered App.js

--- 02tut/src/App.js
+++ 02tut/src/App.js
@@ -2,21 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Kevin";
+  /* jsx stands for javascript xml */
+  /* jsx provides a template for the component layout */
+  /* jsx renders data as text as it displays it */
+
+  const handleNameChange = () => {
+    const names = ['Moose', 'Holly', 'Kevin'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {handleNameChange()}!
+         {/*  Hello {name}! */} {/* Hello Kevin! */}
+          {/* changes what's on the page immediately from here */}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* <p>{ [1,2,3] }</p> */} {/* 123 . Curly braces says this is a JS expression*/}
+       {/*  <p> [1,2,3] </p> */} {/* [1,2,3] */}
+        {/* <p>{{a:1, b:2}}</p> */}  {/*ERROR
+Objects are not valid as a React child (found: object with keys {a, b}). If you meant to render a collection of children, use an array instead.*/}
+        {/* <p>{ true === false }</p> */} {/* react will not render a boolean */}
+       {/*  <p>{name}</p>  */}{/* Kevin */}
       </header>
     </div>
   );
