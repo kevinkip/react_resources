b6d93dafb55c9296d1575401dd6baea164141a05
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-20 21:00:16 -0700

    useEffect Hook w/ localStorage adjust.

--- 02tut/src/App.js
+++ 02tut/src/App.js
@@ -3,18 +3,17 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
 
 const [newItem, setNewItem] = useState('')
 const [search, setSearch] = useState('')
 
-const setAndSaveItems = (newItems) => {
-  setItems(newItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-}
+  useEffect(() => {
+    localStorage.setItem('shoppinglist', JSON.stringify(items));
+  },[items])
 
 const addItem = (item) => {
   const id = items.length 
@@ -23,7 +22,7 @@ const addItem = (item) => {
 
   const myNewItem = { id, checked: false, item };
   const listItems = [...items, myNewItem];
-  setAndSaveItems(listItems);
+  setItems(listItems);
 }
 
 const handleCheck = (id) => {
@@ -33,12 +32,12 @@ const handleCheck = (id) => {
     ? { ...item, checked: !item.checked } 
     : item
   );
-  setAndSaveItems(listItems);
+  setItems(listItems);
 }
 
 const handleDelete = (id) => {
   const listItems = items.filter((item) => item.id !== id); //return list with item.id that equals id. In other words, it filters out the ids that DON'T EQUAL id.
-  setAndSaveItems(listItems);
+  setItems(listItems);
 }
 
 const handleSubmit = (e) => {
